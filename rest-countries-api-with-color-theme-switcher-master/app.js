6373a5ea4ff2303bc827577db6965cab7ce1c083
sddemir <sumeyyedemir1903@gmail.com>
2024-02-27 16:23:47 +0300

    got the name of a country on the page

--- rest-countries-api-with-color-theme-switcher-master/app.js
+++ rest-countries-api-with-color-theme-switcher-master/app.js
@@ -1,1 +1,17 @@
 console.log("Hola Mundo!");
+const search = document.querySelector("#search");
+const body= document.querySelector("#bodyody");
+search.addEventListener('keypress', async function(e) {
+    try {
+        if (e.key === 'Enter') {
+            const mom = search.value;
+            const response = await axios.get(`https://restcountries.com/v3.1/name/${mom}`);
+            console.log(response.data);
+            const div = document.createElement("div");
+            div.innerHTML= response.data[0].altSpellings;
+            document.body.append(div);
+        }
+    } catch (error) {
+        console.log('nope', error);
+    }
+});
