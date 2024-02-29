9216e24249eda201a418a1a3f7cbfc4bda423f73
sddemir <sumeyyedemir1903@gmail.com>
2024-02-29 18:09:48 +0300

    omg frontend

--- rest-countries-api-with-color-theme-switcher-master/app.js
+++ rest-countries-api-with-color-theme-switcher-master/app.js
@@ -29,8 +29,9 @@ search.addEventListener('keypress', async function(e) {
                 countryRegion:response.data[0].continents[0],
                 countryCapital:response.data[0].capital[0]};
             const div = document.createElement("div");
-            div.innerHTML= `
-            <img src="${card.countryFlag}">
+            div.innerHTML= `<div class="row justify-content-center searchCountry">
+            <div class="card">
+            <img src="${card.countryFlag}">            
             <h3>${card.countryName}</h3>
             <p>Population: ${card.countryPop}</p>
             <p>Region: ${card.countryRegion}</p>
